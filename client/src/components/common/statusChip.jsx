64066fa50d50893d229334c8dceb97ad68573743
Drillenissen <logicguy.mailandcontact@gmail.com>
2025-04-13 10:02:04 +0200

    Matched color scheme for statuschip

--- client/src/components/common/statusChip.jsx
+++ client/src/components/common/statusChip.jsx
@@ -1,28 +1,48 @@
 import { Chip } from '@mui/material';
+import { useTheme } from "@mui/material";
+import { tokens } from "../../theme.js";
+import { capitalizeFirstLetter } from '../../utils/strings/capitalizeFirst';
+import CircleIcon from '@mui/icons-material/Circle';
 
 const StatusChip = ({ status }) => {
-  let color = 'default';
-  let label = status;
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
 
-  switch (status) {
-    case 'completed':
-      color = 'success';
-      break;
-    case 'active':
-      color = 'primary';
-      break;
-    case 'expired':
-      color = 'error';
-      break;
-    case 'idle':
-      color = 'warning';
-      break;
-    default:
-      color = 'default';
-      break;
-  }
+  // Define color scheme based on status
+  const getColorScheme = (status) => {
+    switch (status) {
+      case 'completed':
+        return colors.greenAccent;
+      case 'active':
+        return colors.primary;
+      case 'expired':
+        return colors.redAccent;
+      case 'idle':
+        return colors.yellowAccent;
+      default:
+        return colors.grey;
+    }
+  };
 
-  return <Chip label={label} color={color} size="small" />;
+  const colorScheme = getColorScheme(status);
+
+  return (
+    <Chip
+      icon={<CircleIcon />}
+      label={capitalizeFirstLetter(status)}
+      sx={{
+        backgroundColor: colorScheme[900],
+        color: colorScheme[200],
+        border: `1px solid ${colorScheme[200]}`,
+        fontWeight: 600,
+        '& .MuiChip-icon': {
+          color: colorScheme[300],
+          width: "15px"
+        }
+      }}
+      size="small"
+    />
+  );
 };
 
 export default StatusChip;
